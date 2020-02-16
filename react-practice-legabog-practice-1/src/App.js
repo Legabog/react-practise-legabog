54439083e1033a8be72098402e9d3097b87c2ba0
Legabog <probringer@mail.ru>
2020-02-16 16:52:50 +0300

    Add files via upload

---
+++ react-practice-legabog-practice-1/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Layout from './hoc/layout/layout'
+import Quiz from './containers/Quiz/Quiz'
+
+function App() {
+  return (
+    <Layout>
+      <Quiz>
+        
+      </Quiz>
+    </Layout>
+  );
+}
+
+export default App;
