219448d8ef4e83d2bb7af3146fd5200a0aad6b34
legabog <probringer@mail.ru>
2020-03-01 15:35:38 +0300

    Add files via upload

---
+++ react-practice-legabog-practice-3_final_redux_deployed version/build/precache-manifest.148bac1f78d148829ce0ef72b8c2a5a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a88dd542bb4a13d5253abbb8495bb86",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22a1a749cb325da940b7",
+    "url": "/static/css/main.a2f8117a.chunk.css"
+  },
+  {
+    "revision": "c7e41cc4c9c75fc2dba9",
+    "url": "/static/js/2.cbc7e880.chunk.js"
+  },
+  {
+    "revision": "831142632ac8f65e27536d916cf72f8f",
+    "url": "/static/js/2.cbc7e880.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22a1a749cb325da940b7",
+    "url": "/static/js/main.51670f9c.chunk.js"
+  },
+  {
+    "revision": "2ce4909e2e3f906dc99a",
+    "url": "/static/js/runtime-main.642cfca3.js"
+  }
+]);
---
+++ react-practice-legabog-practice-3_final_redux_deployed version/src/components/Navigation/Drawer/Drawer.js
@@ -0,0 +1,65 @@
+import React from 'react'
+import classes from './Drawer.module.css'
+import BackDrop from '../../UI/BackDrop/BackDrop'
+import {NavLink} from 'react-router-dom'
+
+class Drawer extends React.Component {
+
+    clickHandler = () => {
+        this.props.onClose()
+    }    
+
+    renderLinks(links) {
+        return(
+            links.map((link, index) => {
+                return(
+                    <li key={index}>
+                        <NavLink
+                            to={link.to}
+                            exact={link.exact}
+                            activeClassName={classes.active}
+                            onClick={this.clickHandler}
+                        >
+                            {link.label}
+                        </NavLink>
+                    </li>
+                )
+            } )
+        )
+    }
+
+    render() {
+        const cls = [classes.Drawer]
+
+        if(!this.props.isOpen) {
+            cls.push(classes.close)
+        }
+
+        const links = [
+            {to: '/', label: 'Список', exact: true}
+        ]
+
+        console.log('AUth', this.props.isAuthenticated)
+
+        if(this.props.isAuthenticated) {
+            links.push({to: '/quiz-creator', label: 'Создать тест', exact: false})
+            links.push({to: '/logout', label: 'Выйти', exact: false})
+
+        } else {
+            links.push({to: '/auth', label: 'Авторизация', exact: false})
+        }
+
+        return (
+            <React.Fragment>
+                <nav className={cls.join(' ')}>
+                    <ul>
+                        {this.renderLinks(links)}
+                    </ul>
+                </nav>
+                {this.props.isOpen ? <BackDrop onClick={this.props.onClose}/> : null}
+            </React.Fragment>
+        )
+    }
+}
+
+export default Drawer
